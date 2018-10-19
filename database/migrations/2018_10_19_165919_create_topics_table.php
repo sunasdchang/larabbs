0dbfeaed00673a3ce85268e235d1b80d4f13a206
sunasdchang <1729619432@qq.com>
2018-10-19 11:46:00 +0000

    Topic index page

--- database/migrations/2018_10_19_165919_create_topics_table.php
+++ database/migrations/2018_10_19_165919_create_topics_table.php
@@ -9,16 +9,16 @@ class CreateTopicsTable extends Migration
 	{
 		Schema::create('topics', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->index();
-            $table->text('body');
+            $table->string('title')->index();//帖子标题
+            $table->text('body');//帖子内容
             $table->integer('user_id')->unsigned()->index();
             $table->integer('category_id')->unsigned()->index();
-            $table->integer('reply_count')->unsigned()->default(0);
-            $table->integer('view_count')->unsigned()->default(0);
-            $table->integer('last_reply_user_id')->unsigned()->default(0);
-            $table->integer('order')->unsigned()->default(0);
-            $table->text('excerpt')->nullable();
-            $table->string('slug')->nullable();
+            $table->integer('reply_count')->unsigned()->default(0);//回复数量
+            $table->integer('view_count')->unsigned()->default(0);//查看总数
+            $table->integer('last_reply_user_id')->unsigned()->default(0);//最后回复的用户ID
+            $table->integer('order')->unsigned()->default(0);//可用来做排序使用
+            $table->text('excerpt')->nullable();//文章摘要，SEO优化时使用
+            $table->string('slug')->nullable();//SEO友好的URL
             $table->timestamps();
         });
 	}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,7 +9,6 @@ class UsersTableSeeder extends Seeder
     {
         // 获取 Faker 实例
         $faker = app(Faker\Generator::class);
-
         // 头像假数据
         $avatars = [
             'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
@@ -19,7 +18,6 @@ class UsersTableSeeder extends Seeder
             'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
             'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
         ];
-
         // 生成数据集合
         $users = factory(User::class)
             ->times(10)
@@ -30,13 +28,10 @@ class UsersTableSeeder extends Seeder
                 // 从头像数组中随机取出一个并赋值
                 $user->avatar = $faker->randomElement($avatars);
             });
-
         // 让隐藏字段可见，并将数据集合转换为数组
         $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
-
         // 插入到数据库中
         User::insert($user_array);
-
         // 单独处理第一个用户的数据
         $user = User::find(1);
         $user->name = 'Summer';
